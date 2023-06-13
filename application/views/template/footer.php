e91298c10754caac44a728a154198f8856db9ab5
novaliazahra0023 <novaliazahra01@gmail.com>
2023-06-13 03:31:59 -0700

    rubah

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,29 +1,3 @@
-<main>
-				<div class="container-fluid px-4">
-					<h1 class="mt-4">Static Navigation</h1>
-					<ol class="breadcrumb mb-4">
-						<li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-						<li class="breadcrumb-item active">Static Navigation</li>
-					</ol>
-					<div class="card mb-4">
-						<div class="card-body">
-							<p class="mb-0">
-								This page is an example of using static navigation. By removing the
-								<code>.sb-nav-fixed</code>
-								class from the
-								<code>body</code>
-								, the top navigation and side navigation will become static on scroll. Scroll down this
-								page to see an example.
-							</p>
-						</div>
-					</div>
-					<div style="height: 100vh"></div>
-					<div class="card mb-4">
-						<div class="card-body">When scrolling, the navigation stays
-							at the top of the page. This is the end of the static navigation demo.</div>
-					</div>
-				</div>
-			</main>
 			<footer class="py-4 bg-light mt-auto">
 				<div class="container-fluid px-4">
 					<div class="d-flex align-items-center justify-content-between small">
@@ -36,11 +10,11 @@
 					</div>
 				</div>
 			</footer>
-		</div>
-	</div>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
-	</script>
-	<script src="<?= base_url('assets/js/scripts.js')?>"></script>
-</body>
+			</div>
+			</div>
+			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+			</script>
+			<script src="<?= base_url('assets/js/scripts.js')?>"></script>
+			</body>
 
-</html>
+			</html>
