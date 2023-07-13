90b1f0e22eb0a51f3cab09db0675cdf8f01c939f
novaliazahra0023 <novaliazahra01@gmail.com>
2023-07-13 14:12:58 +0800

    praktikum 3

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -9,6 +9,7 @@
                                 Dashboard
                             </a>
                             <div class="sb-sidenav-menu-heading">Interface</div>
+                            
                             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                                 Data Master
